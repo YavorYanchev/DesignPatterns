b15a1df5e2f556d0e5603745b6a1850fbcb9aa70
Yavor Yanchev <Yavor Yanchev@nemetschek.bg>
2018-11-14 13:20:57 +0200

    Added bridge pattern

---
+++ DesignPatterns/Bridge/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Bridge
+{
+	internal class Abstraction
+	{
+		private readonly IBridge _bridge;
+
+		public Abstraction(IBridge implementation)
+		{
+			_bridge = implementation;
+		}
+
+		public string Operation()
+		{
+			return "Abstraction" + " <<< BRIDGE >>>" + _bridge.OperationImpl();
+		}
+	}
+
+	interface IBridge
+	{
+		string OperationImpl();
+	}
+
+	internal class ImplementationA : IBridge
+	{
+		public string OperationImpl()
+		{
+			return "ImplementationA";
+		}
+	}
+
+	internal class ImplementationB : IBridge
+	{
+		public string OperationImpl()
+		{
+			return "ImplementationB";
+		}
+	}
+
+	internal class Program
+    {
+	    private static void Main()
+        {
+	        Console.WriteLine("Bridge Pattern\n");
+	        Console.WriteLine(new Abstraction(new ImplementationA()).Operation());
+	        Console.WriteLine(new Abstraction(new ImplementationB()).Operation());
+        }
+    }
+}

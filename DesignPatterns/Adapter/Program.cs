9cb4365ee9fcc745b2f3df938bfd5f379aa8f462
Yavor Yanchev <yavor_yanchev_bs@abv.bg>
2018-11-28 16:44:06 +0200

    Added adapter example

---
+++ DesignPatterns/Adapter/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Adapter
+{
+
+	//Existing way requests are implemented
+	internal class Adaptee
+	{
+		//provide full precision
+		public double SpecificRequest(double numerator, double denominator)
+		{
+			return numerator / denominator;
+		}
+	}
+
+	internal interface ITarget
+	{
+		//Rough estimate required
+		string Request(int i);
+	}
+
+	internal class Adapter : Adaptee, ITarget
+	{
+		public string Request(int i)
+		{
+			return "Rough estimate is " + (int)Math.Round(SpecificRequest(i, 3));
+		}
+	}
+
+	internal class Client
+	{
+		private static void Main()
+		{
+			//adaptee in standalone mode
+			var adaptee = new Adaptee();
+			Console.WriteLine("Before the new standard\nPrecise reading: ");
+			Console.WriteLine(adaptee.SpecificRequest(5, 3));
+
+			//What the client really wants
+			ITarget adapter = new Adapter();
+			Console.WriteLine("\nMoving to the new standard");
+			Console.WriteLine(adapter.Request(5));
+		}
+	}
+}

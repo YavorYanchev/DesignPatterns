35199095a4f88abd7c8943d80449b72b780c7a4c
Yavor Yanchev <yavor_yanchev_bs@abv.bg>
2018-12-18 18:12:06 +0200

    Add factory example

---
+++ DesignPatterns/Factory/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Factory
+{
+	internal interface IProduct
+	{
+		string ShippedFrom { get; }
+	}
+
+	internal class AvocadoFromSouthAfrica : IProduct
+	{
+		public string ShippedFrom => "South Africa avocado";
+	}
+
+	internal class AvocadoFromSpain : IProduct
+	{
+		public string ShippedFrom => "Spain avocado";
+	}
+
+	internal class NotAvailableProduct : IProduct
+	{
+		public string ShippedFrom => "Not available product";
+	}
+
+	internal class Creator
+	{
+		public IProduct GetProductBasedOnMonth(int month)
+		{
+			if (month >= 4 && month <= 11)
+				return new AvocadoFromSouthAfrica();
+
+			if (month == 1 || month == 2 || month == 12)
+				return new AvocadoFromSpain();
+
+			return new NotAvailableProduct();
+		}
+	}
+
+	internal class Program
+	{
+		private static void Main()
+		{
+			var creator = new Creator();
+
+			for (var i = 1; i <= 12; i++)
+			{
+				var product = creator.GetProductBasedOnMonth(i);
+				Console.WriteLine(product.ShippedFrom);
+			}
+		}
+	}
+}

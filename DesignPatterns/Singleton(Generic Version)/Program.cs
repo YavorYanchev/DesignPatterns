6937bfc17b160d65ba27f6172126308c29e66e1b
Yavor Yanchev <yavor_yanchev_bs@abv.bg>
2018-12-21 16:32:17 +0200

    Add generic version of singleton

---
+++ DesignPatterns/Singleton(Generic Version)/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Singleton_Generic_Version_
+{
+	public class Singleton<T> where T : class, new()
+	{
+		private Singleton() { }
+
+		// ReSharper disable once ClassNeverInstantiated.Local
+		private class SingletonCreator
+		{
+			static SingletonCreator()
+			{
+			}
+
+			//private object instantiated with private constructor
+			internal static readonly T UniqueInstance = new T();
+		}
+
+		public static T Instance => SingletonCreator.UniqueInstance;
+	}
+
+	internal class CoolClass { }
+
+	internal class AnotherClass { }
+
+
+	internal class Client
+	{
+		private static void Main()
+		{
+			var coolInstance = Singleton<CoolClass>.Instance;
+			var secondCoolInstance = Singleton<CoolClass>.Instance;
+
+			var anotherInstance = Singleton<AnotherClass>.Instance;
+
+			if (coolInstance == secondCoolInstance)
+			{
+				Console.WriteLine("Objects are the same instance");
+			}
+
+			Console.WriteLine($"Another instance is of type {anotherInstance.GetType().Name}");
+		}
+	}
+}

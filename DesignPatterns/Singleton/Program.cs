f560c73312bab2997423fbaa4fc04a243fd38324
Yavor Yanchev <yavor_yanchev_bs@abv.bg>
2018-12-21 16:17:23 +0200

    Add singleton example

---
+++ DesignPatterns/Singleton/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Singleton
+{
+	public class Singleton
+	{
+		private Singleton()
+		{
+		}
+
+		// ReSharper disable once ClassNeverInstantiated.Local
+		private class SingletonCreator
+		{
+			static SingletonCreator()
+			{
+			}
+
+			internal static readonly Singleton UniqueInstance = new Singleton();
+		}
+
+		public static Singleton Instance => SingletonCreator.UniqueInstance;
+	}
+
+	internal class Program
+	{
+		private static void Main()
+		{
+			var instance = Singleton.Instance;
+			var secondInstance = Singleton.Instance;
+			Console.WriteLine(instance.Equals(secondInstance));
+			Console.WriteLine($"First instance hash code {instance.GetHashCode()}");
+			Console.WriteLine($"Second instance hash code {secondInstance.GetHashCode()}");
+		}
+	}
+}

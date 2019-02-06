6fe7b8e982974f1e7aa98563c6b9589b6d1d4dda
Yavor Yanchev <yavor_yanchev_bs@abv.bg>
2019-02-06 10:12:26 +0200

    Add Template Method pattern example

---
+++ DesignPatterns/Template Method/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Template_Method
+{
+	internal interface IPrimitives
+	{
+		string Operation1();
+
+		string Operation2();
+	}
+
+	internal class Algorithm
+	{
+		public void TemplateMethod(IPrimitives primitives)
+		{
+			var result = primitives.Operation1() +
+			             primitives.Operation2();
+
+			Console.WriteLine(result);
+		}
+	}
+
+	internal class ClassA : IPrimitives
+	{
+		public string Operation1()
+		{
+			return "ClassA:Op1 ";
+		}
+
+		public string Operation2()
+		{
+			return "ClassA:Op2 ";
+		}
+	}
+
+	internal class ClassB : IPrimitives
+	{
+		public string Operation1()
+		{
+			return "ClassB:Op1 ";
+		}
+
+		public string Operation2()
+		{
+			return "ClassB:Op2 ";
+		}
+	}
+
+	internal class Program
+	{
+		private static void Main()
+		{
+			var algorithm = new Algorithm();
+			algorithm.TemplateMethod(new ClassA());
+			algorithm.TemplateMethod(new ClassB());
+		}
+	}
+}

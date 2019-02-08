552ab2da5de23b5e91a002074577d0cbd0878a33
Yavor Yanchev <yavor_yanchev_bs@abv.bg>
2019-02-08 18:30:29 +0200

    Add template method real-world example

---
+++ DesignPatterns/Exporter Template Method/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Exporter_Template_Method
+{
+	internal abstract class DataExporter
+	{
+		//This will not vary as the data is read from sql only
+		private static void ReadData()
+		{
+			Console.WriteLine("Reading data from SqlServer");
+		}
+
+		//This will not vary as the format of report is fixed
+		private static void FormatData()
+		{
+			Console.WriteLine("Formating the data as per requirements");
+		}
+
+		// This may vary based on target file type chosen
+		protected abstract void ExportData();
+
+		//This is the template method that the client will use
+		public void ExportFormatedData()
+		{
+			ReadData();
+			FormatData();
+			ExportData();
+		}
+	}
+
+	internal class ExcelExporter : DataExporter
+	{
+		protected override void ExportData()
+		{
+			Console.WriteLine("Exporting data to an Excel file.");
+		}
+	}
+
+	internal class PdfExporter : DataExporter
+	{
+		protected override void ExportData()
+		{
+			Console.WriteLine("Exporting data to a PDF file.");
+		}
+	}
+
+	internal class Program
+	{
+		private static void Main()
+		{
+			DataExporter exporter = new ExcelExporter();
+			exporter.ExportFormatedData();
+
+			Console.WriteLine();
+
+			exporter = new PdfExporter();
+			exporter.ExportFormatedData();
+
+		}
+	}
+}

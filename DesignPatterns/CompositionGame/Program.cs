81d44c3288dacf43f35a1c4ed5628b9c7037e0ff
Yavor Yanchev <yavor_yanchev_bs@abv.bg>
2019-04-02 16:35:43 +0300

    Add composition game

---
+++ DesignPatterns/CompositionGame/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CompositionGame
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			// I am not the author of this example
+			// Link to the original https://scottlilly.com/c-design-patterns-composition-over-inheritance/
+			// The author is Scott Lilly
+			// Great post about composition over inheritance!!!
+
+			var camel = MonsterFactory.CreateMonster(MonsterFactory.MonsterType.Camel);
+			Console.WriteLine(camel.CanPunch);
+			Console.WriteLine(camel.SpitDamage);
+		}
+	}
+}
